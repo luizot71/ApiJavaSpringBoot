56ad7fd3faafed5da7e2f3041f62858c83b9ad2f
Luiz Otavio <luizotaviomartinsf@gmail.com>
2020-02-13 13:23:28 -0300

    inserção de string

--- src/main/java/com/br/api/rest/ApiRestApplication.java
+++ src/main/java/com/br/api/rest/ApiRestApplication.java
@@ -8,6 +8,9 @@ public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
+		
+		
+		System.out.println("alterado");
 	}
 
 }
